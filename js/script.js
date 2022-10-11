078bca6a6332a67a3d360f8f490d2788627544f9
Kuat <tipoproger@gmail.com>
2022-10-12 00:07:22 +0600

    calc v1.0

--- js/script.js
+++ js/script.js
@@ -1,35 +1,21 @@
 const resultInput = document.querySelector('.textarea'),
-      buttons = document.querySelectorAll('.calc__btn'),
-      divideBtn = document.querySelector('.calc__btn_divide'),
-      multiplyBtn = document.querySelector('.calc__btn_multiply'),
-      minusBtn = document.querySelector('.calc__btn_minus'),
-      cBtn = document.querySelector('.calc__btn_c'),
-      sevenBtn = document.querySelector('.calc__btn_seven'),
-      eightBtn = document.querySelector('.calc__btn_eight'),
-      nineBtn = document.querySelector('.calc__btn_nine'),
-      plusBtn = document.querySelector('.calc__btn_plus'),
-      fourBtn = document.querySelector('.calc__btn_four'),
-      fiveBtn = document.querySelector('.calc__btn_five'),
-      sixBtn = document.querySelector('.calc__btn_six'),
-      smoothBtn = document.querySelector('.calc__btn_smooth'),
-      oneBtn = document.querySelector('.calc__btn_one'),
-      twoBtn = document.querySelector('.calc__btn_two'),
-      threeBtn = document.querySelector('.calc__btn_three'),
-      zeroBtn = document.querySelector('.calc__btn_zero'),
-      doubleZeroBtn = document.querySelector('.calc__btn_double-zero'),
-      dotBtn = document.querySelector('.calc__btn_c');
+      buttons = document.querySelectorAll('.calc__btn');
       
-function resultElem(btn){
-    if(btn === '='){
-        console.log();
-    }
+function resultElem(){
+    console.log(eval(resultInput.textContent));
+    resultInput.textContent = eval(resultInput.textContent);
+
 }
 
 buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        resultInput.textContent += btn.innerText;
+    btn.addEventListener('click', (e) => {
+        console.log(btn.innerText);
+        if(btn.innerText != '=' && btn.innerText != 'C'){
+            resultInput.textContent += btn.innerText;
+        }
         if(btn.innerText == '='){
-            console.log(+resultInput.textContent);
+            resultElem();
+        }else if(btn.innerText == 'C'){
             resultInput.textContent = '';
         }
     });
